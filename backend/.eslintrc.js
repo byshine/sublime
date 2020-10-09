b05f1e07276ec3d178992bdafbb1f0bbd27962ec
Aaron Ahn <aaron@countable.ca>
2020-10-09 00:39:46 -0700

    updates

---
+++ backend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true
+    },
+    parserOptions: {
+      ecmaVersion: 6
+    },
+    extends: ["prettier", "eslint:recommended"],
+    plugins: ["prettier"]
+};
+  
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+router.prefix('/api')
+
+router.get('/hello', (ctx) => {
+    ctx.body = "Hello Worlds"
+})
+
+app
+ .use(router.routes())
+ .use(router.allowedMethods())
+ .listen(8000);
