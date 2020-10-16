08ca8b9388b25028a744dce0a2ca85197590c1e9
Aaron Ahn <aaron@countable.ca>
2020-10-15 23:29:33 -0700

    change font

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -284,7 +284,7 @@ module.exports = {
         '"Noto Color Emoji"'
       ],
       serif: [
-        "Abhaya Libre",
+        "Neuton",
         "Georgia",
         "Cambria",
         '"Times New Roman"',
