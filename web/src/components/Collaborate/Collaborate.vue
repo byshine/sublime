f0be2575a8abee29f7ee9ea99050074d8d6a27e1
Aaron Ahn <aaron@countable.ca>
2020-11-14 11:47:10 -0800

    changes

--- web/src/components/Collaborate/Collaborate.vue
+++ web/src/components/Collaborate/Collaborate.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="text-white p-10 flex justify-center flex-col items-center">
+    <button @click="handleAuth">Sign In</button>
+    <div id="firebaseui-auth-container"></div>
     <transition
       enter-active-class="translate-opacity duration-200 ease-in"
       leave-active-class="translate-opacity duration-200 ease-in"
@@ -44,6 +46,13 @@ import { Vue, Component } from "vue-property-decorator";
 import SublimeUploader from "@/components/SublimeUploader.vue";
 import UploadCard from "@/components/UploadCard.vue";
 import { collaborate } from "@/api/index.js";
+import firebase from "firebase/app";
+const firebaseui = require("firebaseui");
+var firebaseConfig = {
+  apiKey: "AIzaSyBO0mtp0C1WK1NDo98Ukze4akwaZTL0pYI",
+  authDomain: "classicstyle-2c480.firebaseapp.com",
+  projectId: "classicstyle-2c480"
+};
 
 @Component({
   components: {
@@ -53,6 +62,30 @@ import { collaborate } from "@/api/index.js";
 })
 export default class Collaborate extends Vue {
   files = null;
+
+  created() {
+    firebase.initializeApp(firebaseConfig);
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log("User found", user);
+      } else {
+        console.log("User Not Found");
+      }
+    });
+  }
+
+  handleAuth() {
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    var uiConfig = {
+      signInSuccessUrl: "/collaborate",
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+      ],
+      signInFlow: "popup"
+    };
+    ui.start("#firebaseui-auth-container", uiConfig);
+  }
 
   handleFileChange(files) {
     this.files = files;
