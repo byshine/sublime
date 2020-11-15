fac33ede730580f913db9e2992706fe1d60ad789
Aaron Ahn <aaron@countable.ca>
2020-11-14 22:30:01 -0800

    gallery and about update

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="p-20">Coming Soon</div>
-</template>
+  <div class="w-full h-full flex justify-center items-center p-10 xl:p-2">
+    <div class="text-font-main">
+      <h2 class="text-4xl">the motivation.</h2>
+      <p class="text-md text-left max-w-3xl">
+        Built with <a href="https://vuejs.org">Vue</a>,
+        <a href="https://tailwindcss.com">Tailwind</a> &
+        <a href="https://firebase.google.com">Firebase</a>. Much thanks to the
+        ever-growing JavaScript community. <br />
+        dedicated to the one and only.<br />
+        &#169; Aaron Ahn
+      </p>
+    </div>
+  </div></template
+>
 <script>
 import { Vue, Component } from "vue-property-decorator";
 @Component
--- web/src/views/Gallery.vue
+++ web/src/views/Gallery.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="p-20">Coming Soon</div>
+  <div class="w-full h-full flex justify-center items-center">
+    <p class="text-center">
+      <span class="italic">Coming Soon</span> <br />
+      <span class="italic">2020.</span>
+    </p>
+  </div>
 </template>
 <script>
 import { Vue, Component } from "vue-property-decorator";
