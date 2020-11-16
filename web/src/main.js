f9cce5db8645810d9dd46763fef6644f01071717
Aaron Ahn <aaron@countable.ca>
2020-11-15 23:11:39 -0800

    google tags

--- web/src/main.js
+++ web/src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
 import axios from "axios";
+import VueGtag from "vue-gtag";
 require("object-fit-polyfill");
 console.log(process.env.NODE_ENV);
 axios.defaults.baseURL =
@@ -12,6 +13,12 @@ axios.defaults.baseURL =
     : "http://localhost:5001/classicstyle-2c480/us-central1/widgets/";
 
 Vue.config.productionTip = false;
+
+if (process.env.NODE_ENV === "production") {
+  Vue.use(VueGtag, {
+    config: { id: "G-HF3V4ZQ4WX" }
+  });
+}
 
 new Vue({
   router,
