d4030baf20101db926bd950159607f662c38ac0d
Aaron Ahn <aaron@countable.ca>
2021-01-06 21:24:38 -0800

    exhibition update

--- web/src/views/Gallery.vue
+++ web/src/views/Gallery.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-full h-full flex justify-center items-center">
     <p class="text-center">
-      <span class="italic">Coming Soon</span> <br />
-      <span class="italic">2020.</span>
+      <span class="italic">Vancouver | 2021</span> <br />
+      <span class="italic">Coming soon.</span>
     </p>
   </div>
 </template>
